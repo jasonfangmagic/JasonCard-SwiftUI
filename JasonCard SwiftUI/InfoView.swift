c4dc2050a4945daf159c346bb421d6e0f646ac1e
jasonfangmagic <jasonfangmagic@gmail.com>
2024-04-20 22:47:45 -0400

    add card

---
+++ JasonCard SwiftUI/InfoView.swift
@@ -0,0 +1,18 @@
+//
+//  InfoView.swift
+//  JasonCard SwiftUI
+//
+//  Created by Jason Fang on 4/20/24.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoView()
+}
